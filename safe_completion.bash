8c92a822ac1562d70d8dc61525ab39c3c7e58688
Thomas Mitchell <tmitchell@starkandwayne.com>
2018-01-09 10:11:56 -0500

    Added command name, subcommand names, and subkey support

--- safe_completion.bash
+++ safe_completion.bash
@@ -2,131 +2,230 @@
 
 #Envvars:
 #  _SAFECOMP_PREFIX: Prepends this prefix to each in completion list
-#  _SAFECOMP_NOSPACE_SLASH: Doesn't append space to completion if
-#     completion ends with a slash
+#  _SAFECOMP_NOSPACE: Doesn't append space to completion. Overrides _SAFECOMP_NOSPACE_SLASH
+#  _SAFECOMP_NOSPACE_SLASH: Doesn't append space to completion ending with slash
 __safecomp() {
-	local s IFS=$' '$'\t'$'\n'
-	local cur=${2:-${COMP_WORDS[COMP_CWORD]}}
-	local prefix
-	local space=" "
+  __safe_debug "Entering __safecomp"
 
-	#Old bash installations (like the system bash on mac) have
-	# terrible completion utilities. Let's hack our own `compgen'
-	COMPREPLY=()
-	for s in $1; do
-		local prefixed="${_SAFECOMP_PREFIX}$s"
-		if egrep "^$cur.*" <<<"$prefixed" >/dev/null; then
-			space=" "
-			if [[ -n $_SAFECOMP_NOSPACE_SLASH && \
-			      ${prefixed:${#prefixed}-1:1} == "/" ]]; then
-				space=""
-			fi
+  local s IFS=$' '$'\t'$'\n'
+  local cur=${2:-${COMP_WORDS[COMP_CWORD]}}
 
-			COMPREPLY+=("$prefixed$space")
-		fi
-	done
+  if [[ -n $_SAFECOMP_NOSPACE ]]; then
+    __safe_debug "_SAFECOMP_NOSPACE is set"
+  fi
+
+  if [[ -n $_SAFECOMP_NOSPACE_SLASH ]]; then
+    __safe_debug "_SAFECOMP_NOSPACE_SLASH is set"
+  fi
+
+  if [[ -n $_SAFECOMP_PREFIX ]]; then
+    __safe_debug "_SAFECOMP_PREFIX is \"${_SAFECOMP_PREFIX}\""
+  fi
+
+  #Old bash installations (like the system bash on mac) have
+  # terrible completion utilities. Let's hack our own `compgen'
+  COMPREPLY=()
+  COMP_WORDBREAKS="${COMP_WORDBREAKS//:}"
+  for s in $1; do
+    local prefixed="${_SAFECOMP_PREFIX}$s"
+    if egrep "^$cur.*" <<<"$prefixed" >/dev/null; then
+      local space=" "
+      if [[ -n $_SAFECOMP_NOSPACE ]]; then
+        space=""
+      elif [[ -n $_SAFECOMP_NOSPACE_SLASH && \
+            ${prefixed:${#prefixed}-1:1} == "/" ]]; then
+        space=""
+      fi
+
+      COMPREPLY+=("$prefixed$space")
+    else
+      __safe_debug "Discarding: '$prefixed'"
+    fi
+  done
+
+  if [[ -n $_SAFECOMP_DEBUG ]]; then
+    __safe_debug "Completion options:"
+    __safe_debug "---START---"
+    for option in "${COMPREPLY[@]}"; do
+      __safe_debug "'$option'"
+    done
+    __safe_debug "---END---"
+  fi
 }
 
+#Envvars
+# _SAFECOMP_SUBKEY: If nonempty, this will complete a key if a colon is found
 __safe_complete_path() {
-	local full_path="${COMP_WORDS[$COMP_CWORD]}"
-	if [[ -z $full_path ]]; then
-	  _SAFECOMP_NOSPACE_SLASH=1 __safecomp "secret/"
-	  return 0
-	fi
+  __safe_debug "Completing path"
+  local full_path="${COMP_WORDS[$COMP_CWORD]}"
+  if [[ -z $full_path ]]; then
+    _SAFECOMP_NOSPACE=1 __safecomp "secret/"
+    return 0
+  fi
 
-	local dir="$(dirname $full_path)"
-	if [[ $dir == "." && $full_path != "secret/" ]]; then
-		_SAFECOMP_NOSPACE_SLASH=1 __safecomp "secret/"
-		return 0
-	fi
+  local dir="$(dirname $full_path)"
+  if [[ $dir == "." && $full_path != "secret/" ]]; then
+    _SAFECOMP_NOSPACE=1 __safecomp "secret/"
+    return 0
+  fi
 
-	local base="$(basename $full_path)"
+  local should_nospace=0
 
-	if [[ ${full_path:$((${#full_path} - 1)):1} == "/" ]]; then
-		dir=${full_path:0:$((${#full_path} - 1))}
-		base=""
-	fi
-	dir="${dir}/"
+  if [[ -n $_SAFECOMP_SUBKEY ]]; then
+    if egrep ':' <<<$full_path >/dev/null; then
+      local secret="${full_path/:*}"
+      __safe_debug "...calling safe_complete_key from found-colon path"
+      __safe_complete_key "$secret"
+      return 0
+    fi
 
-  _SAFECOMP_PREFIX="$dir" \
-	_SAFECOMP_NOSPACE_SLASH=1 \
-	__safecomp "$(safe ls "$dir" 2>/dev/null)"
+    should_nospace=1
+  fi
+
+  local base="$(basename $full_path)"
+
+  if [[ ${full_path:$((${#full_path} - 1)):1} == "/" ]]; then
+    dir=${full_path:0:$((${#full_path} - 1))}
+    base=""
+  fi
+  dir="${dir}/"
+
+  if [[ $should_nospace == 1 ]]; then
+    _SAFECOMP_NOSPACE=1 \
+    _SAFECOMP_PREFIX="$dir" \
+      __safecomp "$(safe ls "$dir" 2>/dev/null)"
+  else
+    _SAFECOMP_NOSPACE_SLASH=1 \
+    _SAFECOMP_PREFIX="$dir" \
+      __safecomp "$(safe ls "$dir" 2>/dev/null)"
+  fi
+
+  if [[ -n $_SAFECOMP_SUBKEY && ${#COMPREPLY[@]} -eq 1 && $COMPREPLY == $full_path ]]; then
+    __safe_complete_key "$full_path"
+  fi
 }
 
-__safe_commands() {
-	echo "
-help
-version
-targets
-status
-unseal
-seal
-env
-auth
-login
-renew
-ask
-set
-write
-paste
-exists
-check
-init
-rekey
-get
-read
-cat
-ls
-paths
-tree
-target
-delete
-rm
-export
-import
-move
-rename
-mv
-copy
-cp
-gen
-auto
-ssh
-rsa
-dhparam
-dhparams
-dh
-prompt
-vault
-fmt
-curl
-x509
-"
+__safe_complete_key() {
+  __safe_debug "Completing key"
+  __safe_debug "Checking for keys under secret: $1"
+  __safecomp "$(safe paths --keys "$1")"
 }
 
+# _SAFECOMP_NOHELP: if nonempty, help is omitted from the selection
+_safe_commands() {
+  __safe_debug "Completing command"
+
+  local help="help"
+  if [[ -n $_SAFECOMP_NOHELP ]]; then
+    help=""
+  fi
+  __safecomp "
+${help} version targets status unseal seal env auth login renew ask set write
+paste exists check init rekey get read cat ls paths tree target delete rm
+export import move rename mv copy cp gen auto ssh rsa dhparam dhparams dh
+prompt vault fmt curl x509"
+}
+
+_safe_auth() {
+  __safe_debug "Completing auth"
+  local found
+  while [[ "$_safe_current_token" -lt "$COMP_CWORD" ]]; do
+    local s="${COMP_WORDS[_safe_current_token]}"
+    case "$s" in
+      token|github|ldap|userpass)
+        found=1
+        break
+        ;;
+    esac
+    _safe_current_token="$((++_safe_current_token))"
+  done
+
+  if [[ -z $found ]]; then
+    __safecomp "token github ldap userpass"
+  fi
+}
+
+_safe_x509() {
+  __safe_debug "Completing x509"
+  local cmd
+  while [[ "$_safe_current_token" -lt "$COMP_CWORD" ]]; do
+    local s="${COMP_WORDS[_safe_current_token]}"
+    _safe_current_token="$((++_safe_current_token))"
+    case "$s" in
+      issue|revoke|validate|show)
+        cmd=$s
+        break
+        ;;
+    esac
+  done
+
+  if [[ -z $cmd ]]; then
+    __safecomp "issue revoke validate show"
+    return 0
+  fi
+
+  __safe_complete_path
+}
+
+_safe_target() {
+  target_output=$(safe targets 2>&1 | tail -n +3)
+  __safe_debug "target_output: $target_output"
+
+  local targets=()
+  IFS=$'\n'
+  for line in $target_output; do
+    __safe_debug "line: $line"
+    local target=$(awk '{print $1}' <<<$line )
+    if [[ $target == "(*)" ]]; then
+      target=$(awk '{print $2}' <<<$line )
+    fi
+
+    __safe_debug "target: $target"
+
+    targets+=("$target")
+  done
+
+  __safecomp "${targets[*]}"
+}
+
+__safe_debug() {
+  if [[ -n $_SAFECOMP_DEBUG ]]; then
+    echo "$1" >>/tmp/safecomp_debug
+  fi
+}
 
 _safe() {
-	local i=1
-	local cmd
-  while [[ "$i" -lt "$COMP_CWORD" ]]; do
-		local s="${COMP_WORDS[i]}"
-		case "$s" in
-			-*) #Ignore flags
-				;;
-			*)
-				cmd="$s"
-				break
-				;;
-		esac
-		i="$((++i))"
-	done
+  __safe_debug "Beginning completion"
 
-	if [[ -z $cmd ]]; then
-		cmds=$(__safe_commands)
-		__safecomp "$(__safe_commands)"
-	else
-		__safe_complete_path
-	fi
+  _safe_current_token=1
+  local cmd
+  while [[ "$_safe_current_token" -lt "$COMP_CWORD" ]]; do
+    local s="${COMP_WORDS[_safe_current_token]}"
+    _safe_current_token="$((++_safe_current_token))"
+    case "$s" in
+      -*) #Ignore flags
+        ;;
+      *)
+        cmd="$s"
+        break
+        ;;
+    esac
+  done
+
+  if [[ -z $cmd ]]; then
+    _safe_commands
+    return 0
+  fi
+
+  case "$cmd" in
+    ask|set|write|paste|list|ls|tree|paths|export|rsa|ssh) __safe_complete_path ;;
+    get|read|cat|rm|move|rename|mv|cp|copy|gen|auto) _SAFECOMP_SUBKEY=1 __safe_complete_path ;;
+    x509)                _safe_x509 ;;
+    auth|login)          _safe_auth ;;
+    target)              _safe_target ;;
+    help)                _SAFECOMP_NOHELP=1 _safe_commands ;;
+    *) ;;
+  esac
 }
 
 complete -o nospace -F _safe safe
